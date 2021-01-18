cee2d818b2d3c2228978cff9ed968d2d16acbb3a
AleksanderSkubala <alexskubala@gmail.com>
2021-01-18 15:24:35 +0100

    marking as done

--- src/views/Unit.vue
+++ src/views/Unit.vue
@@ -9,11 +9,23 @@
           <a-badge
             :status="done ? 'success' : (todaySession.content === content ? 'processing' : 'default')"
             :text="`${content}, ${ new Date(date).toLocaleDateString() }`"
+            v-if="todaySession"
+          />
+          <a-badge
+            :status="done ? 'success' : 'default'"
+            :text="`${content}, ${ new Date(date).toLocaleDateString() }`"
+            v-if="!todaySession"
           />
         </li>
       </ul>
       <h3>Session for today:</h3>
       {{todaySession ? todaySession.content : 'No session for today'}}
+      <br/>
+      <br/>
+      <h3>Last not done part</h3>
+      {{notDone ? notDone.content : "Everything's done!"}}
+      <br/>
+      <a-button type="primary" @click="markAsDone">Mark session as done</a-button>
     </div>
   </div>
 </template>
@@ -27,24 +39,45 @@ export default {
     return {
       unitObject: {},
       todaySession: '',
+      notDone: '',
     };
   },
   mounted() {
-    const unitRef = db.collection('units').doc(this.$route.params.id);
-    unitRef.get()
-      .then((doc) => {
-        this.unitObject = doc.data();
-        this.todaySession = this.unitObject.parts.find((part) => {
-          const today = new Date();
-          const partDate = new Date(part.date);
-          return (
-            today.getDate() === partDate.getDate()
-            && today.getMonth() === partDate.getMonth()
-            && today.getFullYear() === partDate.getFullYear()
-          );
-        });
-      })
-      .catch(() => this.$message.error('Something went wrong with database connection...'));
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      const unitRef = db.collection('units').doc(this.$route.params.id);
+      unitRef.get()
+        .then((doc) => {
+          this.unitObject = doc.data();
+          this.todaySession = this.unitObject.parts.find(({ date }) => {
+            const today = new Date();
+            const partDate = new Date(date);
+            return (
+              today.getDate() === partDate.getDate()
+              && today.getMonth() === partDate.getMonth()
+              && today.getFullYear() === partDate.getFullYear()
+            );
+          });
+          this.notDone = this.unitObject.parts.find(({ done }) => !done);
+        })
+        .catch(() => this.$message.error('Something went wrong with database connection...'));
+    },
+    markAsDone() {
+      const notDoneIndex = this.unitObject.parts.findIndex(({ content }) => content === this.notDone.content);
+      const newParts = [...this.unitObject.parts];
+      newParts[notDoneIndex] = {
+        ...this.notDone,
+        done: true,
+      };
+
+      db.collection('units')
+        .doc(this.$route.params.id)
+        .update({ parts: newParts })
+        .then(() => this.fetchData())
+        .catch(() => this.$message.error('Something went wrong with database connection...'));
+    },
   },
 };
 </script>
