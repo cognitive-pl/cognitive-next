3d961d88162d673dbcd9445b237a171efbcb9235
AleksanderSkubala <alexskubala@gmail.com>
2021-03-26 10:16:07 +0100

    support hotfix

--- src/views/Support.vue
+++ src/views/Support.vue
@@ -5,15 +5,18 @@
     <a-row :gutter="[16, 20]" style="margin-top: 30px">
       <a-col :xs="{ span: 20, offset: 1 }" :lg="{ span: 7, offset: 4 }" :xl="{ span: 5, offset: 6 }" >
         <a-card class="card" title="Subscription">
-          <a href="https://py.pl/28bUlP" target="_blank">
+          <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=Y7VCYA25JRYS4" target="_blank">
             <button>5 PLN / month</button>
           </a>
         </a-card>
       </a-col>
       <a-col :xs="{ span: 20, offset: 1 }" :lg="{ span: 7, offset: 2 }" :xl="{ span: 5, offset: 2 }">
-        <a-card class="card" title='"Buy me a coffee"'>
+        <a-card class="card" title="Support once">
           <a href="https://buymeacoffee.com/askubala" target="_blank">
             <button>Buy a coffe ☕</button>
+          </a>
+          <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=HCSTEPKKGVJ3J" target="_blank">
+            <button>10 PLN / once</button>
           </a>
         </a-card>
       </a-col>
@@ -46,6 +49,15 @@ export default {
         background: cornflowerblue;
         color: white;
         font-size: 1.2em;
+      }
+
+      .ant-card-body a {
+        display: block;
+        margin-top: 25px;
+
+        &:first-child {
+          margin-top: 0;
+        }
       }
 
       button {
