80e6af5559d17f7fc0d7770c9b0d43d0c089c776
AleksanderSkubala <alexskubala@gmail.com>
2021-04-02 22:47:58 +0200

    Privacy policy hotfix

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,5 +1,10 @@
 <template>
   <div style="padding: 5vw">
-    <a-button><router-link to="/login">Checkout our app!</router-link></a-button>
+    <router-link to="/login">
+      <a-button type="primary">Check out our app!</a-button>
+    </router-link>
+    <router-link to="/policy">
+      <a-button type="link">Privacy policy</a-button>
+    </router-link>
   </div>
 </template>
